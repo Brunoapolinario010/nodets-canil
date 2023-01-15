89efc7c84a3c8bbbcd6feafe74f0d8dd030b3a1f
Bruno Apolinário <bruno.apolinario010@gmail.com>
2023-01-15 15:14:20 -0300

    Projeto terminado

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -13,7 +15,13 @@ server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
+server.use(mainRoutes);
+
 // Rotas
+
+server.use((req: Request, res: Response) => {
+    res.render('pages/404');
+})
 
 server.listen(process.env.PORT);
 console.log(`🚀 Server listening at: http://localhost:${process.env.PORT}`);
